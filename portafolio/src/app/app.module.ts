50bb09e76ebab232172df4f810173428484d0049
Katherinepaucara <katherinepaucara26@gmail.com>
2022-08-05 16:27:28 -0300

    componente experiencia
    creacion carpeta de eduacion sin contenido

--- portafolio/src/app/app.module.ts
+++ portafolio/src/app/app.module.ts
@@ -8,6 +8,8 @@ import { LogoapComponent } from './components/logoap/logoap.component';
 import { RedesComponent } from './components/redes/redes.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { AcercadeComponent } from './components/acercade/acercade.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,9 @@ import { AcercadeComponent } from './components/acercade/acercade.component';
     LogoapComponent,
     RedesComponent,
     BannerComponent,
-    AcercadeComponent
+    AcercadeComponent,
+    ExperienciaComponent,
+    EducacionComponent
   ],
   imports: [
     BrowserModule,
