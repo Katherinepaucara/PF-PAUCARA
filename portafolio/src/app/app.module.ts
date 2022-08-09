637dcedc08f419c60c997dc64b825746f7f414f5
Katherinepaucara <katherinepaucara26@gmail.com>
2022-08-09 20:35:29 -0300

    eliminando componente REDES
    al unir redes con header se elimina redes

--- portafolio/src/app/app.module.ts
+++ portafolio/src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoapComponent } from './components/logoap/logoap.component';
-import { RedesComponent } from './components/redes/redes.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { AcercadeComponent } from './components/acercade/acercade.component';
 import { ExperienciaComponent } from './components/experiencia/experiencia.component';
@@ -20,7 +19,6 @@ import { FooterComponent } from './components/footer/footer.component';
     AppComponent,
     HeaderComponent,
     LogoapComponent,
-    RedesComponent,
     BannerComponent,
     AcercadeComponent,
     ExperienciaComponent,
