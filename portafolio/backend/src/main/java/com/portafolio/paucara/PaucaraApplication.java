7317e6ede7dd02a0d2831d3846d47ef9b0ec8ae9
Katherinepaucara <katherinepaucara26@gmail.com>
2022-08-14 20:38:32 -0300

    backend01

---
+++ portafolio/backend/src/main/java/com/portafolio/paucara/PaucaraApplication.java
@@ -0,0 +1,13 @@
+package com.portafolio.paucara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaucaraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaucaraApplication.class, args);
+	}
+
+}
---
+++ portafolio/backend/src/test/java/com/portafolio/paucara/PaucaraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portafolio.paucara;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaucaraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
